9c3bc88ae9316768b5693943c860e22a926d43d4
westonludeke <westonludeke@gmail.com>
2021-01-04 13:29:53 -0600

    Added round input logic

--- 2_lesson/rock_paper_scissors_oop.rb
+++ 2_lesson/rock_paper_scissors_oop.rb
@@ -1,3 +1,21 @@
+=begin
+
+The classical approach to object oriented programming is:
+
+1. Write a textual description of the problem or exercise.
+
+2. Extract the major nouns and verbs from the description.
+
+3. Organize and associate the verbs with the nouns.
+
+4. The nouns are the classes and the verbs are the behaviors or methods.
+
+Notice that in OO, we don't think about the game flow logic at all. It's all about organizing and modularizing the code into a cohesive structure - classes. After we come up with the initial class definitions, the final step is to orchestrate the flow of the program using objects instantiated from the classes. We won't worry about that final step yet.
+
+
+
+=end
+
 class Move
   VALUES = ['rock', 'paper', 'scissors']
 
@@ -94,6 +112,18 @@ class RPSGame
     @computer_score = 0
   end
 
+  def select_rounds
+    puts "How many rounds would you like to play?"
+    begin 
+      @rounds = gets.chomp
+      @rounds = Integer(@rounds)
+    rescue ArgumentError
+      puts "Sorry, must enter a number"
+      retry
+    end
+    puts "First to #{@rounds} victories wins the game!"
+  end
+
   def display_goodbye_message
     puts "Thanks for playing Rock, Paper, Scissors. Goodbye!"
   end
@@ -103,7 +133,7 @@ class RPSGame
     puts "#{computer.name} chose #{computer.move}"
   end
 
-  def display_winner
+  def display_round_winner
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -124,6 +154,15 @@ class RPSGame
   def display_score
     puts "#{human.name} now has #{@human_score} points" \
     " and #{computer.name} has #{@computer_score} points"
+    puts "      "
+  end
+
+  def display_game_winner
+    if @human_score == @rounds
+      puts "Congrats #{human.name}! You've won the game!"
+    elsif @computer_score == @rounds
+      puts "#{computer.name} has won the game. Better luck next time!"
+    end
   end
 
   def play_again?
@@ -134,6 +173,7 @@ class RPSGame
       break if ['y', 'n'].include? answer.downcase
       puts "Sorry, must be y or n."
     end
+    system 'clear'
 
     return false if answer.downcase == 'n'
     return true if answer.downcase == 'y'
@@ -141,15 +181,20 @@ class RPSGame
 
   def play
     display_welcome_message
+    select_rounds
     create_empty_scoreboard
 
     loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
-      update_score
-      display_score
+      until @human_score == @rounds || @computer_score == @rounds
+        human.choose
+        computer.choose
+        display_moves
+        display_round_winner
+        update_score
+        display_score
+      end
+      display_game_winner
+      create_empty_scoreboard
       break unless play_again?
     end
     display_goodbye_message
