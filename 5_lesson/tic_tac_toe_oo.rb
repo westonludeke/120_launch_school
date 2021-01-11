82f1309284a831fbeaee3b24a82ec68844ace0b1
westonludeke <westonludeke@gmail.com>
2021-01-11 17:31:43 -0600

    Finished steps 7 & 8 of TTT Game

--- 5_lesson/tic_tac_toe_oo.rb
+++ 5_lesson/tic_tac_toe_oo.rb
@@ -1,39 +1,13 @@
-=begin
-
-We'll take an object-oriented approach to designing the solution, so we'll employ the steps we learned earlier:
-
-  1. Write a description of the problem and extract major nouns and verbs.
-
-  2. Make an initial guess at organizing the verbs into nouns and do a spike to explore the problem with temporary code.
-
-  3. Optional - when you have a better idea of the problem, model your thoughts into CRC cards.
-
---Description--
-
-Tic Tac Toe is a 2-player board game played on a 3x3 grid. Players take turns
-marking a square. The first player to mark 3 squares in a row wins.
-
---Nouns and Verbs--
-
-Nouns: board, player, square, grid
-Verbs: play, mark
-
---Organized--
-
-Board
-Square
-Player
-- mark
-- play
-
-=end
 require 'pry'
 
 class Board
+  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]]              # diagonals
 
   def initialize
     @squares = {}
-    (1..9).each {|key| @squares[key] = Square.new}
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 
   def get_square_at(key)
@@ -51,14 +25,38 @@ class Board
   def full?
     unmarked_keys.empty?
   end
+
+  def someone_won?
+    !!detect_winner
+  end
+
+  def count_human_marker(squares)
+    squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
+  end
+
+  def count_computer_marker(squares)
+    squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
+  end
+
+  # returns winning marker or nil
+  def detect_winner
+    WINNING_LINES.each do |line|
+      if count_human_marker(@squares.values_at(*line)) == 3
+        return TTTGame::HUMAN_MARKER
+      elsif count_computer_marker(@squares.values_at(*line)) == 3
+        return TTTGame::COMPUTER_MARKER
+      end
+    end
+    nil
+  end
 end
 
 class Square
-  INITIAL_MARKER = " "
+  INITIAL_MARKER = ' '
 
   attr_accessor :marker
 
-  def initialize(marker=INITIAL_MARKER)
+  def initialize(marker = INITIAL_MARKER)
     @marker = marker
   end
 
@@ -91,38 +89,39 @@ class TTTGame
   end
 
   def display_welcome_message
-    puts "Welcome to Tic Tac Toe!"
-    puts ""
+    puts 'Welcome to Tic Tac Toe!'
+    puts ''
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+    puts 'Thanks for playing Tic Tac Toe! Goodbye!'
   end
 
   def display_board
     system 'clear'
     puts "You're a #{human.marker}. Computer is a #{computer.marker}"
-    puts ""
-    puts "     |     |"
+    puts ''
+    puts '     |     |'
     puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
     puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
-    puts "     |     |"
-    puts ""
+    puts '     |     |'
+    puts ''
   end
 
   def human_moves
     puts "Choose a square (#{board.unmarked_keys.join(', ')}): "
     square = nil
-    loop do 
+    loop do
       square = gets.chomp.to_i
       break if board.unmarked_keys.include?(square)
+
       puts "Sorry, that's not a valid choice."
     end
 
@@ -135,7 +134,15 @@ class TTTGame
 
   def display_result
     display_board
-    puts "The board is full!"
+
+    case board.detect_winner
+    when human.marker
+      puts 'You won!'
+    when computer.marker
+      puts 'Computer won!'
+    else
+      puts 'It\'s a tie!'
+    end
   end
 
   def play
@@ -143,12 +150,10 @@ class TTTGame
     display_board
     loop do
       human_moves
-      break if board.full?
-      #break if someone_won? || board_full?
+      break if board.someone_won? || board.full?
 
       computer_moves
-      break if board.full?
-      #break if someone_won? || board_full?
+      break if board.someone_won? || board.full?
 
       display_board
     end
@@ -160,9 +165,3 @@ end
 # we'll kick off the game like this
 game = TTTGame.new
 game.play
-
-
-
-
-
-
