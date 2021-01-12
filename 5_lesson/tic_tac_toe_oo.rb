fdce84e70aeb202f70c29107c1ca23dc1449dbef
westonludeke <westonludeke@gmail.com>
2021-01-11 18:18:05 -0600

    Finished step 9 of TTT game

--- 5_lesson/tic_tac_toe_oo.rb
+++ 5_lesson/tic_tac_toe_oo.rb
@@ -7,7 +7,7 @@ class Board
 
   def initialize
     @squares = {}
-    (1..9).each { |key| @squares[key] = Square.new }
+    reset
   end
 
   def get_square_at(key)
@@ -48,6 +48,10 @@ class Board
       end
     end
     nil
+  end
+
+  def reset
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 end
 
@@ -97,8 +101,8 @@ class TTTGame
     puts 'Thanks for playing Tic Tac Toe! Goodbye!'
   end
 
-  def display_board
-    system 'clear'
+  def display_board(clear = true)
+    system 'clear' if clear
     puts "You're a #{human.marker}. Computer is a #{computer.marker}"
     puts ''
     puts '     |     |'
@@ -145,19 +149,42 @@ class TTTGame
     end
   end
 
-  def play
-    display_welcome_message
-    display_board
-    loop do
-      human_moves
-      break if board.someone_won? || board.full?
-
-      computer_moves
-      break if board.someone_won? || board.full?
-
-      display_board
+  def play_again?
+    answer = nil
+    loop do 
+      puts 'Would you like to play again? (y/n)'
+      answer = gets.chomp.downcase
+      break if %w(y n).include? answer
+      puts 'Sorry, must be y or n'
     end
-    display_result
+
+    answer == 'y'
+  end
+
+  def play
+    system 'clear'
+    display_welcome_message
+
+    loop do
+      display_board(false)
+    
+      loop do
+        human_moves
+        break if board.someone_won? || board.full?
+
+        computer_moves
+        break if board.someone_won? || board.full?
+
+        display_board
+      end
+      display_result
+      break unless play_again?
+      board.reset
+      system 'clear'
+      puts 'Let\'s play again'
+      puts ' '
+    end
+
     display_goodbye_message
   end
 end
