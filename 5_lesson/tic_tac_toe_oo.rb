191b9a66198ebe9af56cc2e64a7c9d79b9643c85
westonludeke <westonludeke@gmail.com>
2021-01-12 12:24:49 -0600

    Updated TTT game

--- 5_lesson/tic_tac_toe_oo.rb
+++ 5_lesson/tic_tac_toe_oo.rb
@@ -27,7 +27,7 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   def count_human_marker(squares)
@@ -39,7 +39,7 @@ class Board
   end
 
   # returns winning marker or nil
-  def detect_winner
+  def winning_marker
     WINNING_LINES.each do |line|
       if count_human_marker(@squares.values_at(*line)) == 3
         return TTTGame::HUMAN_MARKER
@@ -101,8 +101,17 @@ class TTTGame
     puts 'Thanks for playing Tic Tac Toe! Goodbye!'
   end
 
-  def display_board(clear = true)
-    system 'clear' if clear
+  def clear
+    system 'clear'
+  end
+
+  def clear_screen_and_display_board
+    clear 
+    display_board
+  end
+
+  def display_board
+    clear
     puts "You're a #{human.marker}. Computer is a #{computer.marker}"
     puts ''
     puts '     |     |'
@@ -137,9 +146,9 @@ class TTTGame
   end
 
   def display_result
-    display_board
+    clear_screen_and_display_board
 
-    case board.detect_winner
+    case board.winning_marker
     when human.marker
       puts 'You won!'
     when computer.marker
@@ -162,11 +171,11 @@ class TTTGame
   end
 
   def play
-    system 'clear'
+    clear
     display_welcome_message
 
     loop do
-      display_board(false)
+      clear_screen_and_display_board
     
       loop do
         human_moves
@@ -175,13 +184,13 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
       board.reset
-      system 'clear'
-      puts 'Let\'s play again'
+      clear
+      puts 'Let\'s play again!'
       puts ' '
     end
 
